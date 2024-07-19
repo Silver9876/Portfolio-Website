803402b582ce729fb40d5a5fd9f97c4116ac6966
Silver9876 <avinashsunka@gmail.com>
2024-07-19 20:33:20 +0530

    commit

--- src/components/education/Certifications.jsx
+++ src/components/education/Certifications.jsx
@@ -5,7 +5,7 @@ import { MdOutlineDeveloperMode } from "react-icons/md";
 
 const Certifications = () => {
   return (
-<div className='w-[100%] h-auto m-20' >
+<div className='w-[100%] h-auto my-20' >
         <h1 className=' text-5xl lg:text-6xl p-12 lg:pl-32 font-bold text-[#C4CFDE] '>Certifications:</h1>
             <div className="flex justify-center">
             <div className='w-[90%] lg:m-10 grid justify-center lg:grid-cols-2 xl:grid-cols-3 gap-y-20'>
