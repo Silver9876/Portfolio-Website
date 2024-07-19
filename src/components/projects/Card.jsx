f3c602da2760df7c0350c7896bda0dd00a3509e2
Silver9876 <avinashsunka@gmail.com>
2024-07-19 15:25:41 +0530

    commit

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -4,9 +4,10 @@ import './Cards.css'
 const Cards = ({image, text, link, title}) => {
   return (
     <div className='outer'>
-      <div className="container min-w-[350px] sm:min-w-[400px] sm:max-w-[500px] sm:max-h-[500px] md:min-w-[500px] md:max-w-[700px] min-h-[400px] lg:max-w-[900px]">
+      {/* min-w-[350px] sm:min-w-[400px] sm:max-w-[500px] sm:max-h-[500px] md:min-w-[500px] md:max-w-[700px] min-h-[400px] lg:max-w-[900px] */}
+      <div className="container w-[90%] lg:w-[100%]">
     <div className="card">
-      <div className="front bg-transparent flex justify-center items-center ">
+      <div className="front bg-transparent flex justify-center items-center h-auto p-10">
         <img src={image}  alt="" className='lg:w-[100%]  rounded-2xl '/>
       </div>
       <div className="back bg-transparent">
