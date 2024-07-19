4dec1e868494675a701f2fb71b0f423531773097
Silver9876 <avinashsunka@gmail.com>
2024-07-19 18:25:00 +0530

    commit

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -16,9 +16,9 @@ const Features = () => {
         <br />
             <div className="flex justify-center h-auto">
             <div className='w-[95%] grid justify-around grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 lg:gap-40'>
-            <Card image={Bussiness} text="Bussiness portfolio" link="https://silver9876.github.io/task3" title="Bussiness portfolio" />
-            <Card image={Fest} text={'Fest website'} link="https://silver9876.github.io/matrix-website/" title={'Fest Website'}/>
-            <Card image={Search} text={'Image Search App'} link="https://silver9876.github.io/image_search_app/" title={'Image Search App'}/>
+            <Card image={Bussiness} text="Internship Project" link="https://silver9876.github.io/task3" title="Bussiness portfolio" />
+            <Card image={Fest} text={'Fest Project'} link="https://silver9876.github.io/matrix-website/" title={'Fest Website'}/>
+            <Card image={Search} text={'Personal Project'} link="https://silver9876.github.io/image_search_app/" title={'Image Search App'}/>
             <Card image={Expense} text={'Open Source Project'} link="https://shrey141102.github.io/Javascript-projects/expense%20tracker/index.html" title={'Expense Tracker'}/>
 
     </div>  
