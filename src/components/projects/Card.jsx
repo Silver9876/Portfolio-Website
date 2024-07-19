9ced53c3df89e96072e480f3b1f032c1682436f5
Silver9876 <avinashsunka@gmail.com>
2024-07-19 15:27:33 +0530

    commit

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -10,7 +10,7 @@ const Cards = ({image, text, link, title}) => {
       <div className="front bg-transparent flex justify-center items-center h-auto p-10">
         <img src={image}  alt="" className='lg:w-[100%]  rounded-2xl '/>
       </div>
-      <div className="back bg-transparent">
+      <div className="back bg-transparent h-auto p-10">
         <h1 className='text-white text-8xl text-center  '>{title}</h1>
         <p>{text}</p>
         <button type='submit' className='w-1/2 h-20 hover:bg-green-700 text-4xl font-bold hover:text-white text-center '><a href={link} className='px-36  hover:text-white text-center'>Website</a></button>
