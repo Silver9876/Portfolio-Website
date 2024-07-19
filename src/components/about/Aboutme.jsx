d34f0d6a9ee012e46b03c876cf7451710feb8b5a
Silver9876 <avinashsunka@gmail.com>
2024-07-19 15:03:33 +0530

    commit

--- src/components/about/Aboutme.jsx
+++ src/components/about/Aboutme.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Aboutme = () => {
   return (
-    <div className='my-32' id='ABOUT'>
-        <h1 className='text-6xl lg:text-8xl font-bold mx-12 lg:mx-32 my-12 '>About Me :</h1>
+    <div className='my-32 lg:my-20' id='ABOUT'>
+        <h1 className='text-6xl lg:text-8xl font-bold mx-12 lg:mx-32 my-12 lg:my-8 '>About Me :</h1>
         <div className="flex justify-center">
         <div className='w-[90%]'>
-      <p className='shadow-xl shadow-black py-14 px-10 rounded-3xl' style={{border:'2px solid white'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aperiam, libero et exercitationem, rem officiis consequuntur perspiciatis iste ducimus ipsam voluptas molestias. Deserunt ex quibusdam, iusto temporibus asperiores quod rem?
+      <p className='shadow-xl shadow-black py-14 px-10 rounded-3xl lg:text-3xl' style={{border:'2px solid white'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aperiam, libero et exercitationem, rem officiis consequuntur perspiciatis iste ducimus ipsam voluptas molestias. Deserunt ex quibusdam, iusto temporibus asperiores quod rem?
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repellendus possimus aliquid quo omnis. Illum ipsum minima ut omnis blanditiis voluptas totam, voluptatem enim quam cupiditate nostrum cum est magni?
       </p>
     </div>
