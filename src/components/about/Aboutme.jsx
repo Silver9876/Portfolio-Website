7548b956c0831a5133816c556c09a81c33967fc3
Silver9876 <avinashsunka@gmail.com>
2024-07-19 15:04:35 +0530

    commit

--- src/components/about/Aboutme.jsx
+++ src/components/about/Aboutme.jsx
@@ -6,7 +6,7 @@ const Aboutme = () => {
         <h1 className='text-6xl lg:text-8xl font-bold mx-12 lg:mx-32 my-12 lg:my-8 '>About Me :</h1>
         <div className="flex justify-center">
         <div className='w-[90%]'>
-      <p className='shadow-xl shadow-black py-14 px-10 rounded-3xl lg:text-3xl' style={{border:'2px solid white'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aperiam, libero et exercitationem, rem officiis consequuntur perspiciatis iste ducimus ipsam voluptas molestias. Deserunt ex quibusdam, iusto temporibus asperiores quod rem?
+      <p className='shadow-xl shadow-black py-14 px-7 rounded-3xl text-lg lg:text-3xl' style={{border:'2px solid white'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aperiam, libero et exercitationem, rem officiis consequuntur perspiciatis iste ducimus ipsam voluptas molestias. Deserunt ex quibusdam, iusto temporibus asperiores quod rem?
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repellendus possimus aliquid quo omnis. Illum ipsum minima ut omnis blanditiis voluptas totam, voluptatem enim quam cupiditate nostrum cum est magni?
       </p>
     </div>
