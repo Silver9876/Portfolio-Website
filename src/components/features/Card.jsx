bca6099eaa7c02966f041041438375a4065a4d17
Silver9876 <avinashsunka@gmail.com>
2024-07-19 15:17:28 +0530

    commit

--- src/components/features/Card.jsx
+++ src/components/features/Card.jsx
@@ -5,7 +5,7 @@ import './Card.css'
 
 const Card = ({Icon, Header, text}) => {
   return (
-    <div className='glass w-[100%] h-auto lg:w-[90%] pb-20 hover:bg-black transition ease-in-out delay-120 hover:-translate-y-1 hover:scale-110 hover:duration-700 ...'>
+    <div className='glass w-[100%] h-auto lg:w-[90%] pb-20 hover:bg-black transition ease-in-out delay-120 hover:-translate-y-1 hover:scale-105 hover:duration-700 ...'>
         <div className='flex flex-col items-start justify-star text-6xl lg:text-8xl pl-20 pt-20 text-green-500'>
         {Icon}
         </div>
