af3a3a777214c48c2d6bf889a86708f26865f554
Avinash <106231902+Silver9876@users.noreply.github.com>
2024-11-05 23:13:31 +0530

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -18,7 +18,7 @@ const Features = () => {
         <br />
             <div className="flex justify-center h-auto">
             <div className='w-[95%] grid justify-around grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 lg:gap-40'>
-            <Card image={Bussiness} text="Internship Project" link="https://silver9876.github.io/task3" title="Bussiness portfolio" />
+            <Card image={Bussiness} text="Internship Project" link="https://it-portfolio-beta.vercel.app/" title="Bussiness portfolio" />
             <Card image={Fest} text={'Fest Project'} link="https://silver9876.github.io/matrix-website/" title={'Fest Website'}/>
             <Card image={Search} text={'Personal Project'} link="https://silver9876.github.io/image_search_app/" title={'Image Search App'}/>
             <Card image={Expense} text={'Open Source Project'} link="https://shrey141102.github.io/Javascript-projects/expense%20tracker/index.html" title={'Expense Tracker'}/>
