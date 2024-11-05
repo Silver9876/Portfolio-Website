2f396e36350eb83057e7733d2a04f1df1ac8187a
Avinash <106231902+Silver9876@users.noreply.github.com>
2024-11-05 17:48:19 +0000

    commit

--- src/components/about/Aboutme.jsx
+++ src/components/about/Aboutme.jsx
@@ -7,7 +7,13 @@ const Aboutme = () => {
         <div className="flex justify-center">
         <div className='w-[90%]'>
       <p className='shadow-xl shadow-black py-8 px-4  lg:py-14 lg:px-10 rounded-3xl text-2xl lg:text-3xl' style={{border:'2px solid white'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aperiam, libero et exercitationem, rem officiis consequuntur perspiciatis iste ducimus ipsam voluptas molestias. Deserunt ex quibusdam, iusto temporibus asperiores quod rem?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repellendus possimus aliquid quo omnis. Illum ipsum minima ut omnis blanditiis voluptas totam, voluptatem enim quam cupiditate nostrum cum est magni?
+      Aspiring Software Engineer
+
+I'm a passionate software engineer with a keen interest in crafting innovative web applications and delving into the fascinating world of machine learning. My expertise lies in leveraging technologies like React and Django to build dynamic and user-friendly web interfaces.
+
+I'm constantly exploring new technologies and trends, particularly in the realm of cybersecurity. I believe that a strong foundation in security principles is essential for building robust and resilient software systems.
+
+I'm excited to contribute my skills and knowledge to challenging projects and collaborate with talented individuals to create impactful solutions.
       </p>
     </div>
         </div>
